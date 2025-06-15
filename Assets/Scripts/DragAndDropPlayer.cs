879bcd13ef0c501d25b2564b5cf2da8a94f016bc
sanguchit0demiga <camilarocio005@gmail.com>
2025-06-15 17:39:47 -0300

    grab and drop mehanic
    + crosshair and anim

---
+++ Assets/Scripts/DragAndDropPlayer.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class DragAndDropPlayer : MonoBehaviour
+{
+    private bool isDragging = false;
+    private Vector3 offset;
+
+    void OnMouseDown()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        offset = transform.position - new Vector3(mousePos.x, mousePos.y, transform.position.z);
+        isDragging = true;
+    }
+
+    void OnMouseUp()
+    {
+        isDragging = false;
+    }
+
+    void Update()
+    {
+        if (isDragging)
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = new Vector3(mousePos.x, mousePos.y, transform.position.z) + offset;
+        }
+    }
+}

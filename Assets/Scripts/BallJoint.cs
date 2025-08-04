a571b789043e42096bc2039d073f1401a0d6cbea
sanguchit0demiga <camilarocio005@gmail.com>
2025-08-04 03:40:42 -0300

    playing mechanic finished
    pipo can play and his entertainment will increase

--- Assets/Scripts/BallJoint.cs
+++ Assets/Scripts/BallJoint.cs
@@ -5,11 +5,18 @@ public class BallJoint : MonoBehaviour
  {
     private TargetJoint2D jointt;
     private Camera cam;
+    public float felicidadPorMovimiento = 5f;
+    public float velocidadMinima = 0.5f;
+    public float tiempoNecesario = 1f;
+
+    private float tiempoEnMovimiento = 0f;
+    private Rigidbody2D rb;
 
     void Start()
     {
         jointt = GetComponent<TargetJoint2D>();
         cam = Camera.main;
+        rb = GetComponent<Rigidbody2D>();
     }
 
     void OnMouseDown()
@@ -33,4 +40,27 @@ public class BallJoint : MonoBehaviour
         Vector3 pos = cam.ScreenToWorldPoint(Input.mousePosition);
         return new Vector2(pos.x, pos.y);
     }
+    void Update()
+    {
+        if (rb.linearVelocity.magnitude > velocidadMinima)
+        {
+            tiempoEnMovimiento += Time.deltaTime;
+
+            if (tiempoEnMovimiento >= tiempoNecesario)
+            {
+                if (StatsPlayer.instance != null)
+                {
+                    StatsPlayer.instance.Jugar(felicidadPorMovimiento);
+                    Debug.Log("¡Pipo está feliz jugando con la pelota!");
+                }
+
+                tiempoEnMovimiento = 0f; 
+            }
+        }
+        else
+        {
+            tiempoEnMovimiento = 0f; 
+        }
+    }
 }
+
